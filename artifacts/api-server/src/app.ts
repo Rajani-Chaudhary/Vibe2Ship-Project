eed4d568286d7534b8f74cb0cccafd5ad9aec43a
Sumit-00621 <sumitsharma21082006@gmail.com>
2026-06-28 17:35:02 +0530

    changes

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,33 +1,32 @@
 import express, { type Express } from "express";
 import cors from "cors";
-// import pinoHttp from "pino-http";
+import pinoHttp from "pino-http";
+import type { IncomingMessage, ServerResponse } from "node:http";
+
 import router from "./routes";
 import { logger } from "./lib/logger";
-// import pinoHttp from "pino-http";
-import { pinoHttp } from "pino-http";
 
 const app: Express = express();
-
 
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: IncomingMessage) {
         return {
-          id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: ServerResponse) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
